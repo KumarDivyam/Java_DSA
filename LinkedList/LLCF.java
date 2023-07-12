8d57d75bd3d4dd63b2290c9aed62ad9051312391
TXM-DOOM <divyamk2025@gmail.com>
2023-07-12 11:24:07 +0530

    Adeed Linked List using Collection Framework

---
+++ LinkedList/LLCF.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class LLCF {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<String>();
+        list.addFirst("This");
+        list.add("is");
+        list.add("a");
+        list.addLast("List");
+        System.out.println(list);
+
+        
+        int ele = list.size();
+        System.out.println(ele);
+
+        for(int i =0 ; i<ele; i++){
+            System.out.println(list.get(i));
+        }
+
+        list.removeFirst();
+        list.removeLast();
+
+        System.out.println(list);
+    }
+}
